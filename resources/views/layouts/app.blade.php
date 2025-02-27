a16a0e3b307e6af7932968cc94f589063f3bbcc2
ruar48 <133135612+ruar48@users.noreply.github.com>
2025-02-27 20:13:11 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title', 'My Laravel App')</title>
+  <!-- Favicon -->
+<link rel="icon" type="image/png" href="{{ asset('dist/img/logo_1.png') }}">
+
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
@@ -191,7 +194,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="#" class="brand-link">
       <img src="{{ asset('dist/img/logo_1.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8; ">
       <span class="brand-text font-weight-light">CodeQuest</span>
     </a>
