a829d799173f573d0423e6a72c3a1c1742e0af54
ruar48 <133135612+ruar48@users.noreply.github.com>
2025-11-05 00:00:54 +0800

    Update engagement.blade.php

--- resources/views/admin/engagement.blade.php
+++ resources/views/admin/engagement.blade.php
@@ -7,7 +7,7 @@
 <style>
 /* === General Page === */
 body, .content-wrapper {
-    background: linear-gradient(135deg, #0a0f24, #1c223a);
+    background: #0d1638ff;
     font-family: 'Poppins', sans-serif;
     color: #2f2f2f;
 }
